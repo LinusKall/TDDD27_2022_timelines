a18212a6de35fc4d5e6e68a03a22fe675ebe4fe4
Linus Käll <linus.kall@nodetron.com>
2022-04-18 19:24:00 +0200

    Made usernames unique

--- database-diesel-postgres/migrations/2022-04-18-115229_create_users/up.sql
+++ database-diesel-postgres/migrations/2022-04-18-115229_create_users/up.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     hashed_password TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
