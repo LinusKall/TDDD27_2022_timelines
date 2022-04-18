0fd594a1bad6d7f30f562f55f3f6743a21bcad96
Linus Käll <linus.kall@nodetron.com>
2022-04-18 19:28:06 +0200

    Created scripts for testing `timelines` table

---
+++ database-diesel-postgres/src/bin/publicize_timeline.rs
@@ -0,0 +1,21 @@
+extern crate database_diesel_postgres as db;
+extern crate diesel;
+
+use self::diesel::prelude::*;
+use self::db::establish_connection;
+use self::db::timelines::*;
+use std::env::args;
+
+fn main() {
+    use db::schema::timelines::dsl::{timelines, public};
+
+    let id = args().nth(1).expect("`publicize_timeline` requires a timeline ID")
+        .parse::<i32>().expect("Invalid ID");
+    let connection = establish_connection();
+
+    let timeline = diesel::update(timelines.find(id))
+        .set(public.eq(true))
+        .get_result::<Timeline>(&connection)
+        .expect(&format!("Unable to find timeline {}", id));
+    println!("Publicized timeline {}", timeline.title);
+}
---
+++ database-diesel-postgres/src/bin/show_timelines.rs
@@ -0,0 +1,22 @@
+extern crate database_diesel_postgres as db;
+extern crate diesel;
+
+use self::db::*;
+use self::timelines::*;
+use self::diesel::prelude::*;
+
+fn main() {
+    use db::schema::timelines::dsl::*;
+
+    let connection = establish_connection();
+    let results = timelines
+        .filter(public.eq(true))
+        .limit(5)
+        .load::<Timeline>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} timelines", results.len());
+    for timeline in results {
+        println!("{}: {}", timeline.id, timeline.title);
+    }
+}
