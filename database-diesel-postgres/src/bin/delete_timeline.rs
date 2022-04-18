6d429f256a1f6f0776bc35b8f74adab80f13750b
Linus Käll <linus.kall@nodetron.com>
2022-04-18 19:36:43 +0200

    Added a deletion script

---
+++ database-diesel-postgres/src/bin/delete_timeline.rs
@@ -0,0 +1,20 @@
+extern crate database_diesel_postgres as db;
+extern crate diesel;
+
+use self::diesel::prelude::*;
+use self::db::*;
+use std::env::args;
+
+fn main() {
+    use db::schema::timelines::dsl::*;
+
+    let target = args().nth(1).expect("Expected a target to match against");
+    let pattern = format!("%{}%", target);
+
+    let connection = establish_connection();
+    let num_deleted = diesel::delete(timelines.filter(title.like(pattern)))
+        .execute(&connection)
+        .expect("Error deleting timeline");
+
+    println!("Deleted {} posts", num_deleted);
+}
