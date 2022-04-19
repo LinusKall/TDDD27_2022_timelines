e7a9014bdea7bbb635d1f1b7fa32acbd565ba334
Linus Käll <linus.kall@nodetron.com>
2022-04-19 17:15:44 +0200

    Moved Clearance enum

--- database-diesel-postgres/src/models/enums.rs
+++ database-diesel-postgres/src/models/enums.rs
@@ -1,9 +1,13 @@
 use diesel_derive_enum::DbEnum;
 
 #[derive(Debug, DbEnum)]
-#[PgType = "clearance"]
+#[DbValueStyle = "SCREAMING_SNAKE_CASE"]
 pub enum Clearance {
     Owner,
     Moderator,
     Subscriber,
+}
+
+pub mod exports {
+    pub use super::Clearance as ClearanceMapping;
 }
