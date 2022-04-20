a873d58bf64ff01e8e0b5e2b5b93299e9d02c120
Sebastian Frisenfelt <sebfr298@student.liu.se>
2022-04-20 15:20:23 +0200

    Made into function-component

--- frontend-yew/src/app/task_list_component/mod.rs
+++ frontend-yew/src/app/task_list_component/mod.rs
@@ -1,56 +1,35 @@
 use yew::prelude::*;
 use web_sys::HtmlInputElement as InputElement;
+use yew::Callback;
 
-
-pub enum TaskListComponentMsg {
-    AddTask(String)
-}
-
-pub struct TaskListComponent {
-    tasks: Vec<String>,
-}
-
-impl Component for TaskListComponent {
-    type Message = TaskListComponentMsg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { tasks: Vec::new() }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, task_list_msg: Self::Message) -> bool {
-        match task_list_msg {
-            TaskListComponentMsg::AddTask(task) => {self.tasks.push(task); true}
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let onkeypress = ctx.link().batch_callback(|e: KeyboardEvent| {
-            if e.key() == "Enter"{
-                let input: InputElement = e.target_unchecked_into();
-                if input.value() != "" {
-                    let value = input.value();
-                    input.set_value("");
-                    Some(Self::Message::AddTask(value))
-                } else {
-                    None
-                }
-            } else {
-                None
+#[function_component(TaskListComponent)]
+pub fn task_list_component() -> Html {
+    let tasks = use_state(|| Vec::new());
+    let title = use_state(|| "Test".to_owned());
+    let onkeypress = Callback::from(|e: KeyboardEvent| {
+        if e.key() == "Enter"{
+            let tasks = tasks.clone();
+            let input: InputElement = e.target_unchecked_into();
+            if input.value() != "" {
+                let value = input.value();
+                input.set_value("");
+                (*tasks).push(value.to_owned());
+            } else { 
             }
-        });
-        html! {
-            <div class="task_list">
-                <h2>{"Timeline_name"}</h2> // Swap this with variable passed down by parent
-                <input
-                    type="new_todo"
-                    placeholder="What needs to be done?"
-                    {onkeypress}
-                />
-                <ul class="item_list">
-                    {for self.tasks.iter().map(|task| html! {<li>{task}</li>})}
-                </ul>
-            </div>
+        } else {
         }
+    });
+    html! {
+        <div class="task_list">
+            <h2>{*title}</h2> 
+            <input
+                type="new_todo"
+                placeholder="What needs to be done?"
+                {onkeypress}
+            />
+            <ul class="item_list">
+                {for (*tasks).iter().map(|task| html! {<li>{task}</li>})}
+            </ul>
+        </div>
     }
 }
