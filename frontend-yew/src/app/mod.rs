b768d22faffaf8c85fd281cc6b32e1ff32fede4a
Sebastian Frisenfelt <sebfr298@student.liu.se>
2022-04-20 15:20:06 +0200

    Made into function-component

--- frontend-yew/src/app/mod.rs
+++ frontend-yew/src/app/mod.rs
@@ -1,28 +1,17 @@
-use yew::prelude::*;
 pub mod task_list_component;
-use task_list_component::*;
 pub mod list_selector_component;
+
+use yew::prelude::*;
+use task_list_component::*;
 use list_selector_component::*;
 
-pub struct App;
 
-pub enum Msg {
-}
-
-impl Component for App {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <div>
-                <ListSelector/>
-                <TaskListComponent/>
-            </div>
-        }
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <div>
+            <ListSelector/>
+            <TaskListComponent/>
+        </div>
     }
 }
