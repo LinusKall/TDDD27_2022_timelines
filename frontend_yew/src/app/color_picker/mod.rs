3d0af9581769603177977b8318ad87d16c1039cb
Edvin Kjell <edvin.kjell@hotmail.com>
2022-04-22 10:06:06 +0200

    Replaced our color picker with a built-in picker

--- frontend_yew/src/app/color_picker/mod.rs
+++ frontend_yew/src/app/color_picker/mod.rs
@@ -10,26 +10,19 @@ pub fn color_picker() -> Html {
         let color = color.clone();
         Callback::from(move |event: Event| {
             let input: HtmlInputElement = event.target_unchecked_into();
-            let name = input.name();
             let value = input.value();
-
-            if let Some(color_val) = value.parse().ok() {
-                match name.as_str() {
-                    "red"   => color.set((color_val, (*color).1, (*color).2)),
-                    "green" => color.set(((*color).0, color_val, (*color).2)),
-                    "blue"  => color.set(((*color).0, (*color).1, color_val)),
-                    _       => {}
-                }
-            }
+            
+            let red   = u8::from_str_radix(&value[1..3], 16).unwrap();
+            let green = u8::from_str_radix(&value[3..5], 16).unwrap();
+            let blue  = u8::from_str_radix(&value[5..7], 16).unwrap();
+            color.set((red, green, blue));
         })
     };
 
     html! {
         <>
             <h2 style={ format!("color: rgb({},{},{});", (*color).0, (*color).1, (*color).2) }>{&*indicator}</h2>
-            <input name="red" placeholder="Red" onchange={onchange.clone()}/>
-            <input name="green" placeholder="Green" onchange={onchange.clone()}/>
-            <input name="blue" placeholder="Blue" onchange={onchange.clone()}/>
+            <input type="color" { onchange }/>
         </>
     }
 }
