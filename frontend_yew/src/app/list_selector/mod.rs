425baf46cfcf055c38b699c2490ac2302a08cc4f
Sebastian Frisenfelt <sebfr298@student.liu.se>
2022-04-22 10:41:23 +0200

    Code cleanup

--- frontend_yew/src/app/list_selector/mod.rs
+++ frontend_yew/src/app/list_selector/mod.rs
@@ -6,36 +6,43 @@ use web_sys::HtmlButtonElement;
 
 #[derive(Debug, Clone, Properties, PartialEq)]
 pub struct Props {
-    pub chosen_timeline: Callback<String>,
+    pub current_timeline: Callback<String>,
 }
 
 #[function_component(ListSelector)]
 pub fn list_selector(props: &Props) -> Html {
-    let timelines = use_state(|| vec!["List1".to_owned(), "List2".to_owned(), "List3".to_owned()]);
-    let timelines_clone = timelines.clone();
+    let timelines = use_state(|| Vec::new());
     // let timeline_context = use_context::<Timeline>();
-    let onkeypress = Callback::from(move |e: KeyboardEvent| {
-        if e.key() == "Enter"{
-            let timelines = timelines_clone.clone();
-            let mut list = (*timelines).clone();
-            let input: InputElement = e.target_unchecked_into();
-            if input.value() != "" {
-                let value = input.value();
-                input.set_value("");
-                list.push(value);
-                timelines.set(list);
-            } else { 
+    // TODO: Read users timelines into timelines.
+    
+    let onkeypress = {
+        let timelines = timelines.clone();
+        Callback::from(move |e: KeyboardEvent| {
+            if e.key() == "Enter"{
+                let mut timeline_list = (*timelines).clone();
+                let input: InputElement = e.target_unchecked_into();
+                if input.value() != "" {
+                    let value = input.value();
+                    input.set_value("");
+                    timeline_list.push(value);
+                    timelines.set(timeline_list);
+                } else { 
+                }
+            } else {
             }
-        } else {
-        }
-    });
-    let chosen_timeline_clone = props.chosen_timeline.clone();
-    let onclick = Callback::from(move |e: MouseEvent| {
-        let target = e.target().unwrap();
-        let input = target.unchecked_into::<HtmlButtonElement>();
-        let value = input.name();
-        chosen_timeline_clone.emit(value);
-    });
+        })
+    };
+
+    let onclick = {
+        let current_timeline = props.current_timeline.clone();
+        Callback::from(move |e: MouseEvent| {
+            let target = e.target().unwrap();
+            let input = target.unchecked_into::<HtmlButtonElement>();
+            let value = input.name();
+            current_timeline.emit(value);
+        })
+    };
+
     html! {
         <div class="list_selector">
             <input
@@ -43,6 +50,7 @@ pub fn list_selector(props: &Props) -> Html {
                 placeholder="Add a new timeline"
                 {onkeypress}
             />
+
             <div class="available_timelines">
                 {
                     for (*timelines)
--- frontend_yew/src/app/task_list/mod.rs
+++ frontend_yew/src/app/task_list/mod.rs
@@ -2,43 +2,48 @@
 use yew::prelude::*;
 use web_sys::HtmlInputElement as InputElement;
 use yew::Callback;
-
 use super::Timeline;
 
 #[function_component(TaskList)]
 pub fn task_list() -> Html {
     let tasks = use_state(|| Vec::new());
     let timeline_context = use_context::<Timeline>();
-    let tasks_clone = tasks.clone();
-    let onkeypress = Callback::from(move |e: KeyboardEvent| {
-        if e.key() == "Enter"{
-            let tasks = tasks_clone.clone();
-            let mut list = (*tasks).clone();
-            let input: InputElement = e.target_unchecked_into();
-            if input.value() != "" {
-                let value = input.value();
-                input.set_value("");
-                list.push(value.to_owned());
-                tasks.set(list);
-            } else { 
+    // TODO: Read from context into tasks here.
+
+    let onkeypress = {
+        let tasks = tasks.clone();
+        Callback::from(move |e: KeyboardEvent| {
+            if e.key() == "Enter"{
+                let mut tasklist = (*tasks).clone();
+                let input: InputElement = e.target_unchecked_into();
+                if input.value() != "" {
+                    let value = input.value();
+                    input.set_value("");
+                    tasklist.push(value.to_owned());
+                    tasks.set(tasklist);
+                } else { 
+                }
+            } else {
             }
-        } else {
-        }
-    });
+        })
+    };
+
     html! {
         <div class="task_list">
             <h2>{timeline_context.unwrap_or_default().name}</h2> 
+
             <input
                 type="new_todo"
-                placeholder="What needs to be done?"
+                placeholder="Add a new task"
                 {onkeypress}
             />
+
             <div class="item_list">
                 { 
                     for (*tasks).iter().map(|task| 
                         html! {
                             <div styles="display: block;">
-                                <input type="checkbox" id={task.clone()} name={task.clone()}/>
+                                <input type="checkbox" class={"checkbox"} id={task.clone()} name={task.clone()}/>
                                 <label for={task.clone()}>{task}</label>
                             </div>
                         }
