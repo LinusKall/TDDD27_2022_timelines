f7623f12ddacbd89635f52d86ae8748db6009bc6
Edvin Kjell <edvin.kjell@hotmail.com>
2022-05-17 11:24:25 +0200

    Added routing to app component

--- frontend_yew/src/app/mod.rs
+++ frontend_yew/src/app/mod.rs
@@ -4,12 +4,81 @@ pub mod list_view;
 pub mod task;
 pub mod task_info;
 pub mod task_list;
+pub mod login;
 
 use list_view::*;
+use login::*;
+
+//------------------------------------Routing-------------
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+pub enum Route {
+    #[at("/login")]
+    Login,
+    #[at("/timelines")]
+    Home,
+    #[at("/secure")]
+    Secure,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component(Secure)]
+fn secure() -> Html {
+    let history = use_history().unwrap();
+
+    let onclick = Callback::once(move |_| history.push(Route::Home));
+    html! {
+        <div>
+            <h1>{ "Secure" }</h1>
+            <button {onclick}>{ "Go Home" }</button>
+        </div>
+    }
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Login => html! {<Login />},
+        Route::Home => html! { <h1>{ "Home" }</h1> },
+        Route::Secure => html! {
+            <Secure />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub type User = Rc<UserInner>;
+
+#[derive(Debug, PartialEq)]
+pub struct UserInner {
+    pub username: RefCell<String>,
+}
+
+//------------------------------------Routing-------------
+
+
 
 #[function_component(App)]
 pub fn app() -> Html {
+
+    let ctx = use_state(|| {
+        Rc::new(UserInner {
+            username: RefCell::new("initial".into()),
+        })
+    });
+
     html! {
-        <ListView/>
+        <ContextProvider<User> context={(*ctx).clone()}>
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+            <Login/>
+        </BrowserRouter>
+        </ContextProvider<User>>
     }
 }
