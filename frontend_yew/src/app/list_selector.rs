4906057880f87e8e02d86edfc041ba2272c8017a
Linus Käll <linus.kall.business@gmail.com>
2022-05-19 20:21:34 +0200

    Made modules files instead of folders

---
+++ frontend_yew/src/app/list_selector.rs
@@ -0,0 +1,66 @@
+use wasm_bindgen::JsCast;
+use web_sys::HtmlButtonElement;
+use web_sys::HtmlInputElement as InputElement;
+use yew::prelude::*;
+// use super::Timeline;
+
+#[derive(Debug, Clone, Properties, PartialEq)]
+pub struct Props {
+    pub current_timeline: Callback<String>,
+}
+
+#[function_component(ListSelector)]
+pub fn list_selector(props: &Props) -> Html {
+    let timelines = use_state(|| Vec::new());
+    // let timeline_context = use_context::<Timeline>();
+    // TODO: Read users timelines into timelines.
+
+    let onkeypress = {
+        let timelines = timelines.clone();
+        Callback::from(move |e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                let mut timeline_list = (*timelines).clone();
+                let input: InputElement = e.target_unchecked_into();
+                if input.value() != "" {
+                    let value = input.value();
+                    input.set_value("");
+                    timeline_list.push(value);
+                    timelines.set(timeline_list);
+                } else {
+                }
+            } else {
+            }
+        })
+    };
+
+    let onclick = {
+        let current_timeline = props.current_timeline.clone();
+        Callback::from(move |e: MouseEvent| {
+            let target = e.target().unwrap();
+            let input = target.unchecked_into::<HtmlButtonElement>();
+            let value = input.name();
+            current_timeline.emit(value);
+        })
+    };
+
+    html! {
+        <div class="list_selector">
+            <input
+                type="new_list"
+                placeholder="Add a new timeline"
+                {onkeypress}
+            />
+
+            <div class="available_timelines">
+                {
+                    for (*timelines)
+                        .clone()
+                        .iter()
+                        .map(|timeline| html! {
+                            <button onclick={onclick.clone()} name={(*timeline).clone()}>{timeline}</button>
+                        })
+                }
+            </div>
+        </div>
+    }
+}
--- frontend_yew/src/backend_graphql_userdata/mod.rs
+++ frontend_yew/src/backend_graphql_userdata/mod.rs
@@ -1,64 +1,64 @@
-use cynic::{http::SurfExt, impl_scalar, QueryBuilder};
-use wasm_bindgen_futures::spawn_local;
-use weblog::console_log;
-use yew::functional::UseStateHandle;
-use yew::prelude::*;
+// use cynic::{http::SurfExt, impl_scalar, QueryBuilder};
+// use wasm_bindgen_futures::spawn_local;
+// use weblog::console_log;
+// use yew::functional::UseStateHandle;
+// use yew::prelude::*;
 
-mod schema {
-    cynic::use_schema!("graphql/schema.graphql");
-}
+// mod schema {
+//     cynic::use_schema!("graphql/schema.graphql");
+// }
 
-type DateTime = chrono::naive::NaiveDateTime;
-impl_scalar!(DateTime, schema::DateTime);
+// type DateTime = chrono::naive::NaiveDateTime;
+// impl_scalar!(DateTime, schema::DateTime);
 
-#[derive(cynic::QueryFragment, Debug)]
-#[cynic(schema_path = "graphql/schema.graphql", graphql_type = "Model")]
-struct Timeline {
-    id: i32,
-    title: String,
-    public: bool,
-    created_at: DateTime,
-    updated_at: DateTime,
-}
+// #[derive(cynic::QueryFragment, Debug)]
+// #[cynic(schema_path = "graphql/schema.graphql", graphql_type = "Model")]
+// struct Timeline {
+//     id: i32,
+//     title: String,
+//     public: bool,
+//     created_at: DateTime,
+//     updated_at: DateTime,
+// }
 
-#[derive(cynic::QueryFragment, Debug)]
-#[cynic(schema_path = "graphql/schema.graphql", graphql_type = "Query")]
-struct Query {
-    get_timelines: Vec<Timeline>,
-}
+// #[derive(cynic::QueryFragment, Debug)]
+// #[cynic(schema_path = "graphql/schema.graphql", graphql_type = "Query")]
+// struct Query {
+//     get_timelines: Vec<Timeline>,
+// }
 
-pub fn full_request(string_handle: &UseStateHandle<String>) {
-    let handle = (*string_handle).clone();
-    use_effect_with_deps(
-        move |_| {
-            let handle = handle.clone();
-            spawn_local(async move {
-                let operation = Query::build(());
+// pub fn full_request(string_handle: &UseStateHandle<String>) {
+//     let handle = (*string_handle).clone();
+//     use_effect_with_deps(
+//         move |_| {
+//             let handle = handle.clone();
+//             spawn_local(async move {
+//                 let operation = Query::build(());
 
-                let res = surf::post("http://localhost/api/graphql")
-                    .run_graphql(operation)
-                    .await
-                    .expect("Could not send request");
+//                 let res = surf::post("http://localhost/api/graphql")
+//                     .run_graphql(operation)
+//                     .await
+//                     .expect("Could not send request");
 
-                let data: Query = res.data.unwrap();
-                console_log!(format!("{:#?}", &data));
-                handle.set(format!("{:?}", &data));
-            });
-            || ()
-        },
-        (),
-    );
-}
+//                 let data: Query = res.data.unwrap();
+//                 console_log!(format!("{:#?}", &data));
+//                 handle.set(format!("{:?}", &data));
+//             });
+//             || ()
+//         },
+//         (),
+//     );
+// }
 
-#[function_component(App)]
-pub fn app() -> Html {
-    let resp = use_state_eq(|| String::from("Hello world!"));
+// #[function_component(App)]
+// pub fn app() -> Html {
+//     let resp = use_state_eq(|| String::from("Hello world!"));
 
-    full_request(&resp);
+//     full_request(&resp);
 
-    html! {
-        <div>
-            { &(*resp) }
-        </div>
-    }
-}
+//     html! {
+//         <div>
+//             { &(*resp) }
+//         </div>
+//     }
+// }
