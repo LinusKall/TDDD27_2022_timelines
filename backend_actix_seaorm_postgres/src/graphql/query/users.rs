ba6672afcd89d2e1e30a0a38801505f8dfd788b8
Linus Käll <linus.kall.business@gmail.com>
2022-05-19 20:20:25 +0200

    Added get_user_id

--- backend_actix_seaorm_postgres/src/graphql/query/users.rs
+++ backend_actix_seaorm_postgres/src/graphql/query/users.rs
@@ -1,6 +1,7 @@
 use async_graphql::{Context, Object, Result};
 use entity::{async_graphql, users};
 use sea_orm::EntityTrait;
+use sea_orm::{entity::*, query::*};
 
 use crate::db::Database;
 
@@ -25,5 +26,26 @@ impl UsersQuery {
             .one(db.get_connection())
             .await
             .map_err(|e| e.to_string())?)
+    }
+
+    async fn get_user_id(
+        &self,
+        ctx: &Context<'_>,
+        username: String,
+        password: String,
+    ) -> Result<Option<i32>> {
+        let db = ctx.data::<Database>().unwrap();
+
+        if let Some(user) = users::Entity::find()
+            .filter(users::Column::Username.eq(username))
+            .filter(users::Column::HashedPassword.eq(password))
+            .one(db.get_connection())
+            .await
+            .map_err(|e| e.to_string())?
+        {
+            Ok(Some(user.id))
+        } else {
+            Ok(None)
+        }
     }
 }
