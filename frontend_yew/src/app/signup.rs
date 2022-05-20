c1b1e3428582e551594728087b1f2bd59354a027
Edvin Kjell <edvin.kjell@hotmail.com>
2022-05-20 15:15:32 +0200

    Implemented signup page

--- frontend_yew/src/app/signup.rs
+++ frontend_yew/src/app/signup.rs
@@ -2,24 +2,83 @@ use web_sys::HtmlInputElement;
 use yew::functional::*;
 use yew::prelude::*;
 use yew_router::prelude::*;
+use weblog::*;
+use regex::Regex;
 
 use super::Route;
 use super::UserId;
 
-// Consolelog
-use weblog::*;
+mod schema {
+    cynic::use_schema!("graphql/schema.graphql");
+}
 
-use regex::Regex;
+#[derive(cynic::FragmentArguments)]
+struct CreateUserInput {
+    username: String,
+    email: String,
+    hashed_password: String,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(
+    schema_path = "graphql/schema.graphql",
+    graphql_type = "Mutation",
+    argument_struct = "CreateUserInput"
+)]
+struct CreateUser {
+    #[arguments(input = &args)]
+    create_user: Option<i32>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(
+    schema_path = "graphql/schema.graphql",
+    graphql_type = "User",
+)]
+struct User {
+    id: i32,
+}
+
+#[derive(Properties, PartialEq)]
+pub struct Properties {
+    pub set_user_id: Callback<i32>,
+}
 
 #[function_component(Signup)]
-pub fn signup() -> Html {
+pub fn signup(props: &Properties) -> Html {
     let username = use_state(String::default);
     let password = use_state(String::default);
     let email = use_state(String::default);
     let valid_email = use_state(bool::default);
+    let validate = Regex::new(r"^[^ ]+@[^ ]+\.[a-z]{2,6}$").unwrap();
     let user_id = use_context::<UserId>().expect("No context found.");
 
-    let validate = Regex::new(r"^[^ ]+@[^ ]+\.[a-z]{2,6}$").unwrap();
+    if let Some(_) = *user_id.borrow() {
+        return html! { <Redirect<Route> to={Route::ListView} /> };
+    }
+
+    let user_id_request = {
+        let set_user_id = props.set_user_id.clone();
+        let username = (*username).to_owned();
+        let hashed_password = (*password).to_owned();
+        let email = (*email).to_owned();
+        let operation = CreateUser::build(CreateUserInput { username, email, hashed_password });
+        use_async(async move {
+            let data = surf::post("http://localhost/api/graphql")
+                .run_graphql(operation)
+                .await
+                .expect("Could not send request")
+                .data
+                .unwrap();
+
+            console_log!(format!("{:#?}", &data));
+            if let Some(User { id }) = data.create_user {
+                set_user_id.emit(id);
+                return Ok(id);
+            }
+            Err("Could not fetch user ID.")
+        })
+    };
 
     let oninput = {
         let current_username = username.clone();
@@ -46,7 +105,14 @@ pub fn signup() -> Html {
         })
     };
 
-    let onclick = { Callback::from(move |_| return) };
+    //let onclick = { Callback::from(move |_| return) };
+
+    let onclick = {
+        let user_id_request = user_id_request.clone();
+        Callback::from(move |_| {
+            user_id_request.run();
+        })
+    };
 
     html! {
         <>
