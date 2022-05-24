1d019454a519d5084537afe5125d4513d3bc414e
Edvin Kjell <edvin.kjell@hotmail.com>
2022-05-24 19:59:05 +0200

    Fixed security issues with nav_bar

--- frontend_yew/src/app/navigation_bar.rs
+++ frontend_yew/src/app/navigation_bar.rs
@@ -15,39 +15,69 @@ pub fn navigation_bar() -> Html {
     let user_id = use_context::<UserId>().expect("No context found.");
     let current_route = use_route::<Route>().unwrap();
     
-    let login_button;
-    let signup_button;
+    let logout_button;
+    let account_info_button;
     let listview_button;
 
     match current_route {
-        Route::Login       => {login_button = false; signup_button = false; listview_button = false; },
-        Route::Signup      => {login_button = false; signup_button = false; listview_button = false; },
-        Route::ListView    => {login_button = true;  signup_button = true;  listview_button = false; },
-        Route::AccountInfo => {login_button = false; signup_button = false; listview_button = true;  },
-        Route::NotFound    => {login_button = false; signup_button = false; listview_button = false; },
+        Route::Login       => {logout_button = false; account_info_button = false; listview_button = false; },
+        Route::Signup      => {logout_button = false; account_info_button = false; listview_button = false; },
+        Route::ListView    => {logout_button = true;  account_info_button = true;  listview_button = false; },
+        Route::AccountInfo => {logout_button = false; account_info_button = false; listview_button = true;  },
+        Route::NotFound    => {logout_button = false; account_info_button = false; listview_button = false; },
     }
 
-    let onclick = {
+    let onclick_logout = {
         let user_id = user_id.clone();
-        Callback::from(move |e: MouseEvent| {
-            let elem: HtmlButtonElement = e.target_unchecked_into();
-            match &elem.name()[..] {
-                "logout"       => {
-                    *user_id.borrow_mut() = None;
-                    LocalStorage::delete(USER_ID_KEY);
-                },
-                "account_info" => {},
-                "listview"     => {},
-                _ => {},
-            }
+        Callback::from(move |_: MouseEvent| {
+            *user_id.borrow_mut() = None;
+            LocalStorage::delete(USER_ID_KEY);
         })
+    };
+
+    let onclick_account_info = {
+        Callback::from(move |_: MouseEvent| {})
+    };
+
+    let onclick_listview = {
+        Callback::from(move |_: MouseEvent| {})
     };
 
     html! {
         <>
-            <Link<Route> to={Route::Login}> <button name={"logout"} onclick={onclick.clone()} hidden={!login_button}>{"Log out"}</button></Link<Route>>
-            <Link<Route> to={Route::AccountInfo}> <button name={"account_info"} onclick={onclick.clone()} hidden={!signup_button}>{"Account information"}</button></Link<Route>>
-            <Link<Route> to={Route::ListView}> <button name={"listview"} onclick={onclick} hidden={!listview_button}>{"Timelines"}</button></Link<Route>>
+            {
+                match logout_button {
+                    true => html! {
+                        <Link<Route> to={Route::Login}> 
+                            <button onclick={onclick_logout}>{"Log out"}</button>
+                        </Link<Route>>
+                    },
+                    false => html! {},
+                }
+            }
+            {
+                match account_info_button {
+                    true => html! {
+                        <Link<Route> to={Route::AccountInfo}> 
+                            <button onclick={onclick_account_info} >{"Account information"}</button>
+                        </Link<Route>>
+                    },
+                    false => html! {},
+                }
+            }
+            {
+                match listview_button {
+                    true => html! {
+                        <Link<Route> to={Route::ListView}> 
+                            <button onclick={onclick_listview} >{"Timelines"}</button>
+                        </Link<Route>>
+                    },
+                    false => html! {},
+                }
+            }
+            /* <Link<Route> to={Route::Login}> <button onclick={onclick_logout} hidden={!logout_button}>{"Log out"}</button></Link<Route>>
+            <Link<Route> to={Route::AccountInfo}> <button onclick={onclick_account_info} hidden={!account_info_button}>{"Account information"}</button></Link<Route>>
+            <Link<Route> to={Route::ListView}> <button onclick={onclick_listview} hidden={!listview_button}>{"Timelines"}</button></Link<Route>> */
         </>
     }
 }
