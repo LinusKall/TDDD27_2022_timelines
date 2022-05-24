018eb0663ce7839d2c29626b502d84a908757062
Linus Käll <linus.kall.business@gmail.com>
2022-05-24 15:21:36 +0200

    Working on tokens table

---
+++ backend_actix_seaorm_postgres/migration/src/m20220524_13340_create_tokens.rs
@@ -0,0 +1,32 @@
+use sea_orm::ConnectionTrait;
+use sea_orm::Statement;
+use sea_orm_migration::prelude::*;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20220524_13340_create_tokens"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let sql = r#"
+        CREATE TABLE tokens (
+            id SERIAL PRIMARY KEY,
+            user_id SERIAL NOT NULL REFERENCES users (id),
+            token TEXT UNIQUE NOT NULL,
+            expires_at TIMESTAMPTZ NOT NULL
+        );"#;
+        let stmt = Statement::from_string(manager.get_database_backend(), sql.to_owned());
+        manager.get_connection().execute(stmt).await.map(|_| ())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let sql = "DROP TABLE tokens";
+        let stmt = Statement::from_string(manager.get_database_backend(), sql.to_owned());
+        manager.get_connection().execute(stmt).await.map(|_| ())
+    }
+}
