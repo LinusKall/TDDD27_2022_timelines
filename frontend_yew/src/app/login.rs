6d3f1f14ea495ddd69daf150ed6765d4d2d1ce05
Edvin Kjell <edvin.kjell@hotmail.com>
2022-05-27 10:43:35 +0200

    Added autofocus and clear input on wrong password

--- frontend_yew/src/app/login.rs
+++ frontend_yew/src/app/login.rs
@@ -21,6 +21,9 @@ pub fn login(props: &Properties) -> Html {
     let username = use_state(|| String::new());
     let password = use_state(|| String::new());
     let user_id = use_context::<UserId>().expect("No context found.");
+    let clear_input = use_state(|| bool::default());
+    let username_ref = use_node_ref();
+    let password_ref = use_node_ref();
     
     if let Some(_) = *user_id.borrow() {
         return html! { <Redirect<Route> to={Route::ListView} /> };
@@ -48,8 +51,10 @@ pub fn login(props: &Properties) -> Html {
 
     let login_click = {
         let user_id_request = user_id_request.clone();
+        let clear_input = clear_input.clone();
         Callback::from(move |_| {
             user_id_request.run();
+            clear_input.set(true);
         })
     };
 
@@ -63,19 +68,51 @@ pub fn login(props: &Properties) -> Html {
 
     let password_input = {
         let current_password = password.clone();
+        let clear_input = clear_input.clone();
         Callback::from(move |e: InputEvent| {
+            clear_input.set(false);
             let input: HtmlInputElement = e.target_unchecked_into();
             current_password.set(input.value());
         })
     };
 
+    let enter_input = {
+        let user_id_request = user_id_request.clone();
+        let clear_input = clear_input.clone();
+        Callback::from(move |e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                user_id_request.run();
+                clear_input.set(true);
+            }
+        })
+    };
+
+
+    let first_render = use_state(|| true);
+    {
+        let first_render = first_render.clone();
+        let username_ref = username_ref.clone(); 
+        let password_ref = password_ref.clone();
+        use_effect(move || {
+            if *first_render {
+                username_ref.cast::<HtmlInputElement>().unwrap().focus().unwrap();
+                first_render.set(false);
+            }
+            if *clear_input {
+                password_ref.cast::<HtmlInputElement>().unwrap().focus().unwrap();
+                password_ref.cast::<HtmlInputElement>().unwrap().set_value("");
+            }
+            || {}
+        });
+    }
+
     html! {
         <>
             <div>
-                <input oninput={username_input} placeholder="Username"/>
+                <input oninput={username_input} onkeypress={enter_input.clone()} ref={username_ref} placeholder="Username"/>
             </div>
             <div>
-                <input oninput={password_input} type="password" placeholder="Password"/>
+                <input oninput={password_input} onkeypress={enter_input} ref={password_ref} type="password" placeholder="Password"/>
             </div>
             <button onclick={
                 login_click
