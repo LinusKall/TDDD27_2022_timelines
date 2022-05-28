ac04039ee2bce047477c33e3cd90fc0b73a4aaca
Linus Käll <linus.kall.business@gmail.com>
2022-05-28 15:15:12 +0200

    Sort tasks by done and created_at

--- backend_actix_seaorm_postgres/src/graphql/query/tasks.rs
+++ backend_actix_seaorm_postgres/src/graphql/query/tasks.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use async_graphql::{Context, Object, Result};
 use entity::{async_graphql, tasks};
 use sea_orm::entity::prelude::*;
@@ -17,9 +19,17 @@ impl TasksQuery {
     ) -> Result<Vec<tasks::Model>> {
         let db = ctx.data::<Database>().unwrap();
 
-        Ok(tasks::Entity::find()
+        let mut tasks = tasks::Entity::find()
             .filter(tasks::Column::TimelineId.eq(timeline_id))
             .all(db.get_connection())
-            .await?)
+            .await?;
+
+        tasks.sort_unstable_by(|a, b| match a.done.cmp(&b.done) {
+            Ordering::Equal => a.created_at.cmp(&b.created_at),
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Less => Ordering::Less,
+        });
+
+        Ok(tasks)
     }
 }
