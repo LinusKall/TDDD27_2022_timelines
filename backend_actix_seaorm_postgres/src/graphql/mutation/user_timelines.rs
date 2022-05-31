8ca4c49db473dfa34f25c1723323df02b03c47f7
Linus Käll <linus.kall.business@gmail.com>
2022-05-31 14:24:18 +0200

    Added update_user_timeline mutation

--- backend_actix_seaorm_postgres/src/graphql/mutation/user_timelines.rs
+++ backend_actix_seaorm_postgres/src/graphql/mutation/user_timelines.rs
@@ -16,6 +16,15 @@ pub struct CreateUserTimelineInput {
     pub public: bool,
 }
 
+#[derive(InputObject)]
+pub struct UpdateUserTimelineInput {
+    props_id: i32,
+    timeline_id: i32,
+    title: Option<String>,
+    color: Option<String>,
+    relation: Option<ClearanceMapping>,
+}
+
 #[derive(SimpleObject)]
 pub struct DeleteUserTimelineResult {
     pub success: bool,
@@ -133,5 +142,67 @@ impl UserTimelinesMutation {
                 })
             }
         }
+    }
+
+    pub async fn update_user_timeline(
+        &self,
+        ctx: &Context<'_>,
+        input: UpdateUserTimelineInput,
+    ) -> Result<UserTimeline> {
+        let db = ctx.data::<Database>().unwrap();
+
+        let timeline = timelines::Entity::find_by_id(input.timeline_id)
+            .one(db.get_connection())
+            .await?
+            .unwrap();
+        let props = timelines_users::Entity::find_by_id(input.props_id)
+            .one(db.get_connection())
+            .await?
+            .unwrap();
+
+        let mut timeline_active: timelines::ActiveModel = timeline.clone().into();
+        let mut props_active: timelines_users::ActiveModel = props.clone().into();
+
+        let mut timeline_update = false;
+        let mut props_update = false;
+
+        if let Some(title) = input.title {
+            timeline_active.title = Set(title.to_owned());
+            timeline_update = true;
+        }
+        if let Some(color) = input.color {
+            props_active.color = Set(color.to_owned());
+            props_update = true
+        }
+        if let Some(relation) = input.relation {
+            props_active.relation = Set(relation.to_owned());
+            props_update = true
+        }
+
+        let timeline: timelines::Model = if timeline_update {
+            timeline_active.updated_at = Set(chrono::offset::Utc::now());
+            timeline_active.update(db.get_connection()).await?
+        } else {
+            timeline
+        };
+        let props: timelines_users::Model = if props_update {
+            props_active.updated_at = Set(chrono::offset::Utc::now());
+            props_active.update(db.get_connection()).await?
+        } else {
+            props
+        };
+
+        Ok(UserTimeline {
+            props_id: props.id,
+            user_id: props.user_id,
+            timeline_id: props.timeline_id,
+            title: timeline.title.to_owned(),
+            relation: props.relation,
+            color: props.color.to_owned(),
+            props_created_at: props.created_at,
+            props_updated_at: props.updated_at,
+            timeline_created_at: timeline.created_at,
+            timeline_updated_at: timeline.updated_at,
+        })
     }
 }
