befed0da662037c77f2419c6fa6e4bb4e4fc7482
Sebastian Frisenfelt <sebfr298@student.liu.se>
2022-06-03 17:36:37 +0200

    restructured task_info

--- frontend_yew/src/app/task_info.rs
+++ frontend_yew/src/app/task_info.rs
@@ -10,55 +10,22 @@ use yew_hooks::use_async;
 
 use super::gql::query::*;
 
+#[derive(Properties, Debug, PartialEq)]
+pub struct Props {
+    pub current_task: Option<Task>,
+    pub update: Callback<UpdateTaskInput>,
+}
+
 #[function_component(TaskInfo)]
-pub fn task_info() -> Html {
-    let task_context = use_context::<Task>();
-    let datetime = use_state(|| task_context.clone().unwrap_or(Task::default()).end_time);
+pub fn task_info(props: &Props) -> Html {
     let timezone = use_state(|| *Local::now().offset());
-    let switched_task = use_state(|| 0);
-    let switched_timeline = use_state(|| 0);
     let body_ref = use_node_ref();
-    let input = use_state(|| UpdateTaskInput {
-        title: None,
-        body: None,
-        done: None,
-        end_time: None,
-    });
-    let body_input = use_state(|| {
-        task_context
-            .clone()
-            .unwrap_or(Task::default())
-            .body
-            .unwrap_or("".to_owned())
-            .is_empty()
-    });
+    let body_input = use_state(|| props.current_task.is_some() && props.current_task.as_ref().unwrap().body.is_none());
 
-    let update_task = {
-        let task_id = task_context.clone().unwrap_or(Task::default()).id;
-        let input = input.clone();
-        use_async(async move {
-            let operation = UpdateTask::build(UpdateTaskArguments {
-                task_id,
-                input: input.deref().clone(),
-            });
-            let data = surf::post(format!("{}/api/graphql", crate::app::LOCALHOST))
-                .run_graphql(operation)
-                .await
-                .expect("Could not create User Timeline")
-                .data;
-
-            if let Some(t) = data {
-                return Ok(t.update_task);
-            }
-            Err("Could not create User Timeline.")
-        })
-    };
-
-    let task_datetime = {
-        let datetime = datetime.clone();
-        let input = input.clone();
-        let update_task = update_task.clone();
+    let update_end_time = {
         let timezone = timezone.clone();
+        let update = props.update.clone();
+        let task_id = props.current_task.clone().unwrap().id;
         Callback::from(move |e: Event| {
             let value = e
                 .target()
@@ -67,39 +34,47 @@ pub fn task_info() -> Html {
                 .value();
             let value = format!("{}:00{}", value, &*timezone);
             let newtime = DateTime::parse_from_rfc3339(&value);
-            datetime.set(Some(newtime.unwrap().with_timezone(&Utc)));
-            let update = UpdateTaskInput {
+            update.emit(UpdateTaskInput {
+                task_id,
                 title: None,
                 body: None,
                 done: None,
                 end_time: Some(newtime.unwrap().with_timezone(&Utc)),
-            };
-            input.set(update);
-            update_task.run();
+            });
         })
     };
 
     let update_body = {
+        let update = props.update.clone();
+        let task_id = props.current_task.clone().unwrap().id;
         let body_input = body_input.clone();
-        let input = input.clone();
-        let update_task = update_task.clone();
         Callback::from(move |k: KeyboardEvent| {
             let value = k.target_unchecked_into::<HtmlInputElement>().value();
-            if k.shift_key() && k.key() == "Enter" && !value.is_empty() {
-                let update = UpdateTaskInput {
-                    title: None,
-                    body: Some(value),
-                    done: None,
-                    end_time: None,
-                };
-                input.set(update);
-                update_task.run();
-                body_input.set(false);
+            if k.shift_key() && k.key() == "Enter"{
+                if value.is_empty() {
+                    update.emit(UpdateTaskInput {
+                        task_id,
+                        title: None,
+                        body: None,
+                        done: None,
+                        end_time: None,
+                    });
+                    body_input.set(true);
+                } else {
+                    update.emit(UpdateTaskInput {
+                        task_id,
+                        title: None,
+                        body: Some(value),
+                        done: None,
+                        end_time: None,
+                    });
+                    body_input.set(false);
+                }
             }
         })
     };
 
-    let ondblclick = {
+    let make_mutable = {
         let body_input = body_input.clone();
         Callback::from(move |_: MouseEvent| {
             body_input.set(true);
@@ -119,34 +94,14 @@ pub fn task_info() -> Html {
     };
 
     {
-        let switched_task = switched_task.clone();
-        let datetime = datetime.clone();
-        let task_context = task_context.clone();
-        let body_input = body_input.clone();
         let body_ref = body_ref.clone();
         use_effect(move || {
-            let cur_task = task_context.clone().unwrap();
-            if *switched_task != cur_task.id || *switched_timeline != cur_task.timeline_id {
-                datetime.set(cur_task.end_time);
-                switched_task.set(cur_task.id);
-                switched_timeline.set(cur_task.timeline_id);
-                body_input.set(
-                    task_context
-                        .clone()
-                        .unwrap_or(Task::default())
-                        .body
-                        .unwrap_or("".to_owned())
-                        .is_empty(),
-                )
-            }
-
             if let Some(elem) = body_ref.cast::<HtmlInputElement>() {
                 elem.style().set_property("height", "0").unwrap();
                 elem.style()
                     .set_property("height", &format!("{}px", elem.scroll_height()))
                     .unwrap();
             }
-
             || {}
         })
     }
@@ -154,13 +109,13 @@ pub fn task_info() -> Html {
     html! {
         <div class="task_info">
             {
-                if task_context.clone().unwrap_or(Task::default()).id != 0 {
+                if props.current_task.is_some() {
                     html! {
                         <>
-                            <h2>{task_context.clone().unwrap_or(Task::default()).title}</h2>
+                            <h2>{&props.current_task.as_ref().unwrap().title}</h2>
                             {
                                 if *body_input {
-                                    let cur_task = task_context.clone().unwrap();
+                                    let cur_task = props.current_task.clone().unwrap();
                                     html! {
                                         <>
                                             <p><b>{"Description: "}</b></p>
@@ -176,13 +131,13 @@ pub fn task_info() -> Html {
                                         </>
                                     }
                                 } else {
-                                    let cur_task = task_context.clone().unwrap();
+                                    let cur_task = props.current_task.clone().unwrap();
                                     html! {
                                         <>
                                             <p><b>{"Description (Double click to edit): "}</b></p>
                                             <p
                                                 class="task_body"
-                                                {ondblclick}>
+                                                ondblclick={make_mutable}>
                                                 {cur_task.body.clone().unwrap_or("".to_owned())}
                                             </p>
                                         </>
@@ -190,27 +145,27 @@ pub fn task_info() -> Html {
                                 }
                             }
                         {
-                            if let Some(datetime) = datetime.deref().clone() {
+                            if let Some(datetime) = props.current_task.as_ref().unwrap().end_time.clone() {
                                 let datetime = datetime + *timezone;
                                 let datetime = datetime.to_rfc3339();
                                 let datetime = &datetime[0..datetime.len()-6];
                                 html! {
                                         <>
                                             <p><b>{"Deadline: "}</b></p>
-                                            <input name={"endtime"} type="datetime-local" value={datetime.to_owned()} onchange={task_datetime}/>
+                                            <input name={"endtime"} type="datetime-local" value={datetime.to_owned()} onchange={update_end_time}/>
                                         </>
                                     }
                             } else {
                                 html! {
                                     <>
                                         <p><b>{"Deadline: "}</b></p>
-                                        <input name={"endtime"} type="datetime-local" onchange={task_datetime}/>
+                                        <input name={"endtime"} type="datetime-local" onchange={update_end_time}/>
                                     </>
                                 }
                             }
                         }
                         {
-                            if task_context.clone().unwrap_or(Task::default()).done == true {
+                            if props.current_task.as_ref().unwrap().done {
                                 html! {<h3>{"Completed"}</h3>}
                             }
                             else {
