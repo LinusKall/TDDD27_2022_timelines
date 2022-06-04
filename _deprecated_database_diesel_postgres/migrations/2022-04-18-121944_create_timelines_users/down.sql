d3aaaef69007a9b5727a805c17627d3f3d5a9079
Linus Käll <linus.kall.business@gmail.com>
2022-06-04 10:25:38 +0200

    Renamed deprecated crates

---
+++ _deprecated_database_diesel_postgres/migrations/2022-04-18-121944_create_timelines_users/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE timelines_users;
+DROP TYPE clearance_mapping;
---
+++ _deprecated_database_diesel_postgres/src.bak/bin/events_show.rs
@@ -0,0 +1,20 @@
+extern crate database_diesel_postgres as db;
+extern crate diesel;
+
+use self::db::establish_connection;
+use self::db::table::events::*;
+use self::diesel::prelude::*;
+
+fn main() {
+    use db::schema::events::dsl::*;
+
+    let connection = establish_connection();
+    let results = events
+        .load::<Event>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} events", results.len());
+    for event in results {
+        println!("{:?}", event);
+    }
+}
